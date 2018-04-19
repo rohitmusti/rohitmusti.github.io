89b2c5b771b7c293682e065f2b621dd569cf6a9b
rohitmusti <rohit.musti.rm@gmail.com>
2018-04-19 00:56:02 -0400

    set it up for michaels

--- js/drinks.js
+++ js/drinks.js
@@ -1,15 +1,25 @@
 function randomDrink(data) {
-  // for (i = 0; i < data.length; i++) {
-  //     ret += `<div id='card->`
-  //     ret += data[i]
-  //     ret += `</div>`
-  //   }
-
   var d = new Date();
   var n = d.getTime();
 
-  var ret = `<div id='cards'>`
-  ret += data[n % 999].FIELD5
+  var ret = `<div id='name'> Drink: `
+  ret += data[n % data.length].FIELD1
+  ret += `</div>`
+
+  ret += `<div id='alc'> Percent Alcohol: `
+  ret += data[n % data.length].FIELD2
+  ret += `</div>`
+
+  ret += `<div id='size'> Size (it matters): `
+  ret += data[n % data.length].FIELD3
+  ret += `</div>`
+
+  ret += `<div id='price'> Price: `
+  ret += data[n % data.length].FIELD4
+  ret += `</div>`
+
+  ret += `<div id='description'> Description: `
+  ret += data[n % data.length].FIELD5
   ret += `</div>`
 
   return ret;
---
+++ js/michaels.js
@@ -0,0 +1,506 @@
+drinks = [
+ {
+   "FIELD1": "3 Notch’d Hydraulion Red",
+   "FIELD2": "5.30%",
+   "FIELD3": "$6.00",
+   "FIELD4": "16oz",
+   "FIELD5": "This local Irish-Style Red is a perfectly balanced beer with a great caramel sweetness alongside a tangy, citrusy hop profile.",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "PBR",
+   "FIELD2": "4.74%",
+   "FIELD3": "$3.00",
+   "FIELD4": "16oz",
+   "FIELD5": "It’s Back!",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Ballast Point Coconut Victory at Sea",
+   "FIELD2": "10%",
+   "FIELD3": "$7.00",
+   "FIELD4": "16oz",
+   "FIELD5": "This Coconut Ballast Point Victory at Sea takes their popular Imperial Porter to another place, a tropical place. They added sweet roasted coconut to their robust porter brewed with Caffe Calabria coffee and vanilla,  the perfect complement to this beer’s subtly roasty intensity",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Firestone Walker Pivo Pilsner",
+   "FIELD2": "5.30%",
+   "FIELD3": "$6.00",
+   "FIELD4": "14oz",
+   "FIELD5": "Pivo Hoppy Pils is a classically rendered pilsner with a West Coast dry-­‐hopping twist, showcasing stylistic inﬂuences from Germany, Italy and the Czech Republic. Lighter beer styles like pilsner have been hijacked by industrial lager beer in the United States, and it’s time for craft brewers to take it back. Pivo Hoppy Pils oﬀers impeccable balance with ﬂoral aromatics, spicy herbal nuances, and bergamot zest and lemongrass notes from dry hopping with German Saphir hops",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Oskar Blues Dale’s Pale Ale",
+   "FIELD2": "6.50%",
+   "FIELD3": "$6.50",
+   "FIELD4": "16oz",
+   "FIELD5": "A voluminously hopped mutha of a pale ale. If you’re looking for a fistful of flavor, look no further than this hopped-up trail ride.  It delivers a hoppy nose and assertive-but-balanced flavors of pale malts and citrusy floral hops from the first sip to the final swig",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Ommegang Rosetta",
+   "FIELD2": "5.60%",
+   "FIELD3": "$7.00",
+   "FIELD4": "12oz",
+   "FIELD5": "Ale aged on cherries. Traditional Belgian Kriek, pleasingly tart with a touch of sweetness. Brewed exclusively for Brewery Ommegang by Liefmans, a sister brewery, in Belgium",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Schlafly Tasmanian IPA",
+   "FIELD2": "7.20%",
+   "FIELD3": "$7.00",
+   "FIELD4": "16oz",
+   "FIELD5": "Tasmanian IPA is brewed with 100% Australian hops. The Tasmanian hop, Galaxy, is a strong, aromatic variety that is different from traditional strains of the Pacific Northwest. Their brewers developed a recipe that showcases the hop’s unique flavors of citrus and pineapple",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Reason Saison",
+   "FIELD2": "4.50%",
+   "FIELD3": "$6.50",
+   "FIELD4": "12oz",
+   "FIELD5": "A take on the Grisette (a centuries-old Belgian and French saison) honors the heritage of the style while adding a subtle modern hoppiness. The result is a beer that seamlessly blends the traditional estery yeast and dry finish with an American hop flavor",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Alewerks Bitter Valentine",
+   "FIELD2": "8.30%",
+   "FIELD3": "$8.00",
+   "FIELD4": "16oz",
+   "FIELD5": "A poured glass of Bitter Valentine is full of intentions and optimistic expectations. An empty glass of Bitter Valentine is getting dumped on February 13th, left to wonder if you will ever love again.  Alewerks repeats the cycle every year. Not especially challenging as the name implies, the flavor is exceptionally balanced and smooth with notes of citrus and pine from Pacific Northwest hops",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Austin East Pineapple Cider",
+   "FIELD2": "5.00%",
+   "FIELD3": "$6.00",
+   "FIELD4": "16oz",
+   "FIELD5": "For an exotic twist, they have blended heirloom bittersweet and culinary apples with a taste of the tropics. Golden, ripe, and delicious pineapples have been married into their dry cider to give you a refreshing pineapple experience",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Blue Mountain Kolsch 151",
+   "FIELD2": "5.00%",
+   "FIELD3": "$5.50",
+   "FIELD4": "12oz",
+   "FIELD5": "A light German-style ale indigenous to Cologne. This ale is treated like a lager, undergoing extensive cold aging to produce a clean, crisp beer.",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Blue Mountain Full Nelson Pale Ale",
+   "FIELD2": "5.90%",
+   "FIELD3": "$5.50",
+   "FIELD4": "12oz",
+   "FIELD5": "A world class beer from just 20 minutes away.  A must try!",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Blue Mountain Dark Hollow",
+   "FIELD2": "10.00%",
+   "FIELD3": "$10.50",
+   "FIELD4": "375ml cork and cage",
+   "FIELD5": "A complex Imperial stout age and charred in American oak bourbon barrels.",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Bold Rock Virginia Draft Cider",
+   "FIELD2": "4.70%",
+   "FIELD3": "$5.00",
+   "FIELD4": "12oz",
+   "FIELD5": "A not too sweet, not too dry cider that drinks like a light lager.  Smooth and mellow.",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Bold Rock IPA",
+   "FIELD2": "4.70%",
+   "FIELD3": "$6.00",
+   "FIELD4": "12oz",
+   "FIELD5": "Bold Rock India Pressed Apple’s new west coast style is a dry-hopped hard cider with citrus and floral notes.",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Champion Shower Beer",
+   "FIELD2": "4.50%",
+   "FIELD3": "$5.50",
+   "FIELD4": "12oz",
+   "FIELD5": "The Bohemian Pilsner sings with clean maltiness and spicy flavor. If you have never had a shower beer it is high time!",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Devil’s Backbone Vienna Lager",
+   "FIELD2": "4.90%",
+   "FIELD3": "$5.00",
+   "FIELD4": "12oz",
+   "FIELD5": "Chestnut colored lager with a malty aroma and subtle toasty sweetness.  A pinch of noble hops gives way to a refreshing, clean finish.",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Foothills Jade",
+   "FIELD2": "7.40%",
+   "FIELD3": "$7.00",
+   "FIELD4": "12oz",
+   "FIELD5": "Bold, citrusy Pacific Jade hops lend striking tropical fruit notes and a hint of peppery finish to this easy-drinking IPA. Dry hop additions of Chinook and Citra add heady aroma and bold flavor.",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Parkway Get Bent",
+   "FIELD2": "7.20%",
+   "FIELD3": "$5.50",
+   "FIELD4": "12oz",
+   "FIELD5": "Light bodied, west coast-styled IPA with a delightful bready character balanced by lots of citrusy hops.",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Three Notch’d Hydraulion Red",
+   "FIELD2": "5.30%",
+   "FIELD3": "$5.00",
+   "FIELD4": "12oz",
+   "FIELD5": "One of our local faves! Smooth and silky with light to medium carbonation, medium body, and a mildly sweet aftertaste.",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Potters Grapefruit Hibiscus",
+   "FIELD2": "9.50%",
+   "FIELD3": "$11.50",
+   "FIELD4": "500ml",
+   "FIELD5": "With juicy grapefruit aromas, bright acidity from the Gold Rush apples, and a pale pink color from the hibiscus petals, and zero residual sugar this cider is refreshing and very drinkable. Beware the high ABV!",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Lagunitas Hop Stoopid",
+   "FIELD2": "8.00%",
+   "FIELD3": "$9.00",
+   "FIELD4": "22oz",
+   "FIELD5": "Cheap, good, beer",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Rochefort 8",
+   "FIELD2": "9.20%",
+   "FIELD3": "$10.50",
+   "FIELD4": "12oz",
+   "FIELD5": "Cheap, good, beer",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Founders KBS",
+   "FIELD2": "12.40%",
+   "FIELD3": "$10.00",
+   "FIELD4": "12oz",
+   "FIELD5": "We have it! Only a few left so get in here before it’s gone! $10",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Founder’s Dirty Bastard",
+   "FIELD2": "8.50%",
+   "FIELD3": "$5.50",
+   "FIELD4": "12oz",
+   "FIELD5": "So good it’s almost wrong.  Dark ruby in color, brewed with seven varieties of imported malts.  Complex in finish, with hints of smoke and peat, paired with a malty richness and a good amount of hops.  Ain’t for the wee lads",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Lagunitas Little Sumpin’ Sumpin’",
+   "FIELD2": "7.50%",
+   "FIELD3": "$5.50",
+   "FIELD4": "12oz",
+   "FIELD5": "Well balanced with sweet malt and citrus grapefruit, finishing with a nice wheat flavor ",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Blue Mountain Full Nelson Pale",
+   "FIELD2": "5.90%",
+   "FIELD3": "$6.00",
+   "FIELD4": "12oz",
+   "FIELD5": "A world class beer from just 20 minutes away. A must try!",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Bell’s Amber Ale ",
+   "FIELD2": "5.50%",
+   "FIELD3": "$5.50",
+   "FIELD4": "12oz",
+   "FIELD5": "Perfectly balanced and so easy to drink",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Deschutes Fresh Squeezed IPA",
+   "FIELD2": "6.40%",
+   "FIELD3": "$6.00",
+   "FIELD4": "12oz",
+   "FIELD5": "A juicy citrus and grapefruit profile.  As is fresh Citra and Mosaic hops were squeezed straight into the bottle",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Bell’s Two Hearted IPA",
+   "FIELD2": "7.00%",
+   "FIELD3": "$6.00",
+   "FIELD4": "12oz",
+   "FIELD5": "Nicely balanced with a citrusy floral hop bitterness",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Ballast Point Grapefruit Sculpin",
+   "FIELD2": "7.00%",
+   "FIELD3": "$7.50",
+   "FIELD4": "12oz",
+   "FIELD5": "Though there are so few ways to improve on Ballasat Point’s signature IPA (Sculpin), the tart freshness of the grapefruit perfectly complements the Sculpin’s citrusy hop character ",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Parkway Get Bent IPA",
+   "FIELD2": "7.20%",
+   "FIELD3": "$5.50",
+   "FIELD4": "22oz",
+   "FIELD5": "Light bodied, west coast-styled IPA with a delightful bready character balanced by lots of citrusy hops",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Foothills Jade IPA",
+   "FIELD2": "7.40%",
+   "FIELD3": "$7.00",
+   "FIELD4": "12oz",
+   "FIELD5": "Bold, citrusy Pacific Jade hops lend striking tropical fruit notes and a hint of peppery finish to this easy-drinking IPA.  Dry hop additions of Chinook and Citra add heady aroma and bold flavor",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "AleSmith Nut Brown Ale",
+   "FIELD2": "5.00%",
+   "FIELD3": "$7.00",
+   "FIELD4": "12oz",
+   "FIELD5": "Deep mahogany in color, rich, malt-forward flavor balances notes of biscuit, mild cocoa and earthy hops. A creamy mouthfeel and full body makes it an extremely sessionable ale.",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Deschutes Black Butte Porter",
+   "FIELD2": "5.20%",
+   "FIELD3": "$5.50",
+   "FIELD4": "12oz",
+   "FIELD5": "Notes of rich chocolate and cofee, a luscious creaminess and a roasted finish",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Left Hand Milk Stout Nitro",
+   "FIELD2": "6.00%",
+   "FIELD3": "$6.50",
+   "FIELD4": "12oz",
+   "FIELD5": "Dark and delicious, America’s great milk stout will change your perception about what a stout can be.  Super smooth with soft roastiness and mocha flavors",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Rogue Shakespeare Oatmeal Stout",
+   "FIELD2": "5.80%",
+   "FIELD3": "$7.50",
+   "FIELD4": "12oz",
+   "FIELD5": "Deeply roasted and creamy, with tantalizing bitterness; thick, smooth, and attractive; deeply satisfying.",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "AleSmith Speedway Stout",
+   "FIELD2": "12.00%",
+   "FIELD3": "$16.00",
+   "FIELD4": "750ml",
+   "FIELD5": "Ominous and pitch-black with chocolate and roasted malt flavors, notes of dark fruit, toffee, and caramel. A healthy dose of coffee is added to each batch bringing out the beer’s dark chocolate flavors and enhancing it’s drinkability.  Despite its intensity, Speedway Stout’s fine carbonation and creamy mouthfeel make it very smooth and surprisingly easy to drink.  A world-class stout and one of our favorites.",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Stella Artois",
+   "FIELD2": "5.20%",
+   "FIELD3": "$5.50",
+   "FIELD4": "Ask Server",
+   "FIELD5": "Ask Server",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Natty Boh",
+   "FIELD2": "4.28%",
+   "FIELD3": "$3.00",
+   "FIELD4": "Ask Server",
+   "FIELD5": "Ask Server",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Bud",
+   "FIELD2": "5.00%",
+   "FIELD3": "$4.00",
+   "FIELD4": "Ask Server",
+   "FIELD5": "Ask Server",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Eggenberg Pils",
+   "FIELD2": "5.10%",
+   "FIELD3": "$5.50",
+   "FIELD4": "11.2oz",
+   "FIELD5": "A crisp and refreshing Austrian pilsner",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Crispin Blackberry Pear Cider",
+   "FIELD2": "6.00%",
+   "FIELD3": "$6.00",
+   "FIELD4": "12oz",
+   "FIELD5": "Naturally elegant, refreshingly adult with an authentic blackberry dark-fruit taste and a sweet-sharp fresh tang.  Mouthwatering juicy complexity.  Luscious pear-berry bouquet",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Fruli Strawberry Wit Bier",
+   "FIELD2": "4.10%",
+   "FIELD3": "$7.50",
+   "FIELD4": "11.2oz",
+   "FIELD5": "A lightly hopped Belgian wit bier with strawberries delivers a soft and refreshing indulgence that is simply delicious.",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Green Flash Passion Fruit Kicker",
+   "FIELD2": "5.50%",
+   "FIELD3": "$5.50",
+   "FIELD4": "12oz",
+   "FIELD5": "A jaw-dropping, mouth-watering, smooth brew with sweet, tart, fruity flavor.",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "J.K. Scrumpy’s Cider",
+   "FIELD2": "6.00%",
+   "FIELD3": "$10.00",
+   "FIELD4": "22oz",
+   "FIELD5": "Fresh from the orchard and organic, this hand-bottled cider has been around since the 1850’s. ",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Schillings Ascender Ginger Cider",
+   "FIELD2": "6.50%",
+   "FIELD3": "$7.00",
+   "FIELD4": "12oz",
+   "FIELD5": "Fermented with fresh California ginger giving it a nice burst of flavor and a crisp finish.",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Schillings Lumberjack Rhubarb Cider",
+   "FIELD2": "6.00%",
+   "FIELD3": "$7.00",
+   "FIELD4": "12oz",
+   "FIELD5": "A semi-dry cider with moderate tartness and acidity, no bitterness, sourness, or tannins, and notes of rhubarb, pear, tropical fruit, peach, honey, and apple. A bit champagne-like, though less carbonated, this cider is light bodied with smooth buttery texture ",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Victory Golden Monkey",
+   "FIELD2": "9.50%",
+   "FIELD3": "$6.50",
+   "FIELD4": "12oz",
+   "FIELD5": "A full bodied Belgian tripel w/ a nice balance between malt and spicyness. Beware the monkey!!",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Delirium Tremens",
+   "FIELD2": "9.00%",
+   "FIELD3": "$9.50",
+   "FIELD4": "12oz",
+   "FIELD5": "Elected best beer in the world",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Duchesse de Bourgogne",
+   "FIELD2": "6.20%",
+   "FIELD3": "$9.50",
+   "FIELD4": "11.2oz",
+   "FIELD5": "Ahh the Duchesse… sour cherry and green apple are characteristics of this Flemish sour red ale",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Trappistes Rochefort 10",
+   "FIELD2": "11.30%",
+   "FIELD3": "$13.00",
+   "FIELD4": "11.2oz",
+   "FIELD5": "Extremely complex, almost smoky with lots of dark fruits **Availability of this beer is extremely limited**",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Chimay Blue",
+   "FIELD2": "9.00%",
+   "FIELD3": "$10.50",
+   "FIELD4": "11.2oz",
+   "FIELD5": "A Belgian strong dark ale that is very smooth and malty",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Schneider Aventinus",
+   "FIELD2": "8.00%",
+   "FIELD3": "$8.50",
+   "FIELD4": "16.9oz",
+   "FIELD5": "Hints of banana with a wheat tang and a little hop bitterness, a top quality wheat doppelbock",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Allagash White",
+   "FIELD2": "5.20%",
+   "FIELD3": "$7.00",
+   "FIELD4": "12oz",
+   "FIELD5": "Brewed with a generous portion of wheat and spiced with coriander and Curaçao orange peel. This beer is fruity, refreshing, and slightly cloudy in appearance.",
+   "FIELD6": "",
+   "FIELD7": ""
+ },
+ {
+   "FIELD1": "Paulaner Hefe Weiss",
+   "FIELD2": "5.50%",
+   "FIELD3": "$5.50",
+   "FIELD4": "11.2oz",
+   "FIELD5": "Germany’s #1 wheat beer and one of the world’s favorites; beer experts call it a masterpiece.",
+   "FIELD6": "",
+   "FIELD7": ""
+ }
+]
