9214bd153bb2a94700fc343f369f6973486bccc7
rohitmusti <rohit.musti.rm@gmail.com>
2020-11-01 01:17:06 -0400

    almost there on portfolio

--- test/src/components/Portfolio/Portfolio.js
+++ test/src/components/Portfolio/Portfolio.js
@@ -1,28 +1,34 @@
 import "./Portfolio.css";
 import { Fragment, Component } from "react";
-import { Card } from "antd";
+import { Card, Col, Row, Carousel } from "antd";
+
+function onChange(a, b, c) {
+  console.log(a, b, c);
+}
 
 class Portfolio extends Component {
   constructor(props) {
     super(props);
+    let projectData = require("../../assets/projects.json");
     this.state = {
-      landingLocation: {
-        lat: 41.850033,
-        lng: -87.6500523,
-        zoom: 4,
-      },
-      currentPointOfInterest: null,
-      places: placesData,
+      projects: projectData,
     };
   }
 
   render() {
-    return (
-      <Fragment>
-        <h1>Hey</h1>
-        <p>This is some text</p>
-      </Fragment>
-    );
+    let prep = [];
+    Object.keys(this.state.projects).map((key, index) => {
+      let currentProj = this.state.projects[key];
+      prep.push(
+        <div className="contentStyle">
+          <h3>{currentProj.name}</h3>
+          <a href={currentProj.link}>Link</a>
+          <p style={{ color: "white" }}>{currentProj.description}</p>
+        </div>
+      );
+    });
+
+    return <Carousel afterChange={onChange}>{prep}</Carousel>;
   }
 }
 
