529b6a013c73cf29b4d75a2305315d2f7fdbcb44
rohitmusti <rohit.musti.rm@gmail.com>
2022-11-13 14:12:49 -0800

    working at non profit vs a start up

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -26,20 +26,21 @@ export default function Blog({
 }: {
   posts: [{ slug: string; frontmatter: { date: string; title: string } }];
 }) {
+  posts.sort((a, b) => a.frontmatter.date.localeCompare(b.frontmatter.date));
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-4 md:p-0">
       {posts.map(({ slug, frontmatter }) => (
-        <div
-          key={slug}
-          className="border-2 border-white m-2 rounded-lg flex flex-col"
-        >
-          <Link href={`/blog/${slug}`}>
+        <Link href={`/blog/${slug}`}>
+          <div
+            key={slug}
+            className="border-2 border-white m-2 rounded-lg flex flex-col hover:bg-red-700"
+          >
             <a>
               <h1 className="font-semibold p-2">{frontmatter.title}</h1>
             </a>
-          </Link>
-          <p className="p-2">Date: {frontmatter.date}</p>
-        </div>
+            <p className="p-2">Date: {frontmatter.date}</p>
+          </div>
+        </Link>
       ))}
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -22,41 +22,19 @@ const Home: NextPage = () => {
         <h1>Hi!</h1>
         <br />
         <p className="">
-          My name is Rohit and I am software and data engineer hoping to use my
-          technical skills to improve the world! I work at{" "}
+          My name is Rohit and I am software and data engineer! I work at{" "}
           <a
-            href="https://americanforests.org/"
+            href="https://read.ai/"
             target="_blank"
             className="underline"
             rel="noreferrer"
           >
-            American Forests
-          </a>{" "}
-          and bring projects like the{" "}
-          <a
-            href="https://treeequityscore.org"
-            target="_blank"
-            className="underline"
-            rel="noreferrer"
-          >
-            Tree Equity Score
-          </a>{" "}
-          to fruition. Next Spring (2022), I&apos;ll begin teching an
-          undergraduate course on Cryptography through the{" "}
-          <a
-            href="https://www.techtalentpipeline.nyc/tech-in-residence-corps#:~:text=The%20Tech%2Din%2DResidence%20Corps,need%20to%20enter%20the%20workforce.&text=Tech%2Din%2DResidence%20Corps%20members,next%20generation%20of%20NYC%20students."
-            target="_blank"
-            className="underline"
-            rel="noreferrer"
-          >
-            Tech in Residence Corps
-          </a>{" "}
-          at CUNY.
-        </p>
-        <p>
-          Outside of work, I enjoy learning the ins and outs of bike maintenance
-          by volunteering at my local free bike clinic in the farmers market. I
-          also enjoy rock climbing, tennis, and chess.
+            Read AI
+          </a>
+          {", "}
+          working to create meeting tools for the 21st century. Outside of work,
+          I enjoy tinkering and riding bikes, climbing (mostly falling off of)
+          rocks, playing tennis, and enjoy chess!
         </p>
       </div>
     </div>
