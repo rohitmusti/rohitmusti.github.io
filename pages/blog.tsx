e338249d9c3afcc5b33a8d3faa1fab4cbe7b30e3
Rohit Musti <rohitmusti@Rohits-MacBook-Pro.local>
2025-01-01 19:39:32 -0800

    reorganize block and add life cycle of jira ticket

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -21,19 +21,12 @@ export async function getStaticProps() {
   };
 }
 
-export default function Blog({
-  posts,
-}: {
-  posts: [{ slug: string; frontmatter: { date: string; title: string } }];
-}) {
-  posts.sort((a, b) => a.frontmatter.date.localeCompare(b.frontmatter.date));
+export default function Blog({ posts }: { posts: [{ slug: string; frontmatter: { date: string; title: string } }] }) {
+  posts.sort((a, b) => b.frontmatter.date.localeCompare(a.frontmatter.date));
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-4 md:p-0">
       {posts.map(({ slug, frontmatter }) => (
-        <div
-          key={slug}
-          className="border-2 border-white m-2 rounded-lg flex flex-col hover:bg-red-700"
-        >
+        <div key={slug} className="border-2 border-white m-2 rounded-lg flex flex-col hover:bg-red-700">
           <Link href={`/blog/${slug}`}>
             <a>
               <h1 className="font-semibold p-2">{frontmatter.title}</h1>
